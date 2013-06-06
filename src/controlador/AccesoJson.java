bb84a417af824ec4ef4aa588dad99c16a4b4da12
Arturo Cadena Romero <arturo.cr88@gmail.com>
2013-06-06 15:03:08 -0500

    Clase JSONPaser y JSONPaserImplement

--- src/controlador/AccesoJson.java
+++
@@ -1,5 +0,0 @@
-package controlador;
-
-public class AccesoJson {
-
-}
---
+++ src/controlador/JSONParserImplement.java
@@ -0,0 +1,30 @@
+package com.Graphsraphs;
+
+import java.util.HashMap;
+
+public class JSONParserImplement {
+	
+	private HashMap<String, String> urlValue = new HashMap<String, String>();
+	
+	
+	public JSONParserImplement(){
+		
+		urlValue.put("", "");
+		urlValue.put("", "");
+		urlValue.put("", "");
+		urlValue.put("", "");
+		urlValue.put("","");
+		urlValue.put("", "");
+		urlValue.put("", "");
+		urlValue.put("", "");
+	}
+	
+	
+	public void getStaticValuesFromJSON(){
+		
+		//JSONParser jParser = new JSONParser();
+		
+		
+	}
+
+}
